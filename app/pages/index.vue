3ebf83e15d834ffffa03903b971083e79524107d
Navid Talebian <navidt81@gmail.com>
2026-04-02 12:48:20 +0330

    tiny improvement

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,59 +1,50 @@
 <template>
-	<div class="flex items-center justify-center py-12 h-dvh">
-		<div class="flex max-w-xl justify-around items-center gap-4 flex-col">
-			<div class="">
-				<div
-					ref="inputsEl"
-					class="grid grid-cols-4 mb-2"
-				>
-					<UInput
-						v-for="n in range(16)"
-						v-model="letters[n]"
-						:key="n"
-						size="xl"
-						color="info"
-						maxlength="1"
-						class="max-w-15 p-1 rounded-md"
-						:class="{
-							'bg-primary': selectedPath.includes(n),
-							'bg-secondary': selectedPath[0] == n,
-						}"
-						@click="deleteWord([n])"
-						@keyup.delete="deleteWord(letters[n] ? [n] : [n - 1])"
-						@input="nextTick(() => focusFirstEmptyInput(letters.indexOf('')))"
-					/>
-				</div>
-				<div class="flex w-full *:w-full *:block *:text-center gap-6">
-					<UButton @click="deleteWord(selectedPath)">تایید</UButton>
+	<div class="flex items-center justify-center flex-col py-12 h-dvh gap-3">
+		<div
+			ref="inputsEl"
+			class="grid grid-cols-4"
+		>
+			<UInput
+				v-for="n in range(16)"
+				v-model="letters[n]"
+				:key="n"
+				size="xl"
+				class="max-w-15 p-1 rounded-md"
+				:class="{
+					'bg-primary': selectedPath.includes(n),
+					'bg-secondary': selectedPath[0] == n,
+				}"
+				@click="deleteWord([n])"
+				@keyup.delete="deleteWord(letters[n] ? [n] : [n - 1])"
+				@input="nextTick(() => focusFirstEmptyInput(letters.indexOf('')))"
+			/>
+		</div>
+		<div class="flex w-full *:w-full *:block *:text-center gap-2 max-w-60">
+			<UButton @click="deleteWord(selectedPath)">تایید</UButton>
+			<UButton
+				color="error"
+				@click="deleteWord(range(16))"
+			>
+				پاک
+			</UButton>
+		</div>
+		<div class="grid grid-cols-3 gap-2 min-h-40">
+			<UButton
+				v-for="{ score, word, path } in findWords(letters)"
+				:key="word"
+				:color="isEqual(path, selectedPath) ? 'neutral' : 'primary'"
+				@click="selectedPath = path"
+			>
+				<div class="flex justify-between w-full items-center gap-2">
+					{{ word }}
 					<UButton
-						color="error"
-						@click="deleteWord(range(16))"
+						size="sm"
+						:color="score > 6 ? 'success' : score > 4 ? 'warning' : 'error'"
 					>
-						پاک
+						{{ score }}
 					</UButton>
 				</div>
-			</div>
-			<div class="grid grid-cols-3 gap-2 min-h-40">
-				<UButton
-					v-for="{ score, word, path } in findWords(letters)"
-					:key="word"
-					class="cursor-pointer max-h-10"
-					type="button"
-					:color="isEqual(path, selectedPath) ? 'neutral' : 'primary'"
-					@click="selectedPath = path"
-				>
-					<div class="flex justify-between w-full items-center gap-2">
-						{{ word }}
-						<UButton
-							size="sm"
-							as="div"
-							:color="score > 6 ? 'success' : score > 4 ? 'warning' : 'error'"
-						>
-							{{ score }}
-						</UButton>
-					</div>
-				</UButton>
-			</div>
+			</UButton>
 		</div>
 	</div>
 </template>
