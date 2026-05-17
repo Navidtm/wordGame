59820b9d9c912a9d4b9ff3c95e516e4add588a18
Navid Talebian <navidt81@gmail.com>
2026-05-17 19:04:02 +0330

    extract textField Component from main

---
+++ app/components/textFields.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { range } from 'es-toolkit';
+
+const letters = defineModel<string[]>({ required: true });
+
+const { path } = defineProps<{ path: number[] }>();
+
+const inputs = useTemplateRef('inputs');
+
+const parseInput = (n: number) => {
+	const char = letters.value[n]!;
+	if (persianMap[char]) letters.value[n] = persianMap[char];
+
+	if (!/[ا-ی]/.test(letters.value[n]!)) letters.value[n] = '';
+};
+
+const deleteChar = (n: number) => (letters.value[n] = '');
+
+onMounted(() => inputs.value?.[0]?.focus());
+
+watch(letters.value, (v) => {
+	const idx = v.indexOf('');
+	if (idx >= 0) inputs.value?.[idx]?.focus();
+});
+</script>
+<template>
+	<div
+		class="grid grid-cols-4 bg-primary/60 rounded-lg p-4 gap-2 w-fit mx-auto"
+	>
+		<input
+			v-for="n in range(16)"
+			v-model="letters[n]"
+			ref="inputs"
+			ref_for
+			:key="n"
+			class="rounded-md border border-black/30 w-14 h-12 text-center transition-all hover:opacity-80"
+			:class="path.includes(n) ? 'bg-gray-700' : 'bg-gray-800'"
+			maxlength="1"
+			@click="deleteChar(n)"
+			@keyup.delete="deleteChar(letters[n] ? n : n - 1)"
+			@input="nextTick(() => parseInput(n))"
+		/>
+	</div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,38 +1,21 @@
 <script setup lang="ts">
 import { isEqual, range, fill } from 'es-toolkit';
 
-const inputs = useTemplateRef('inputs');
-
 const letters = ref(fill(Array(16), ''));
-const focusedButton = ref(0);
-
 const words = computed(() => findWords(letters.value));
-const path = computed(() => words.value[focusedButton.value]?.[1] ?? []);
+
+const selectedButton = ref(0);
+const path = computed(() => words.value[selectedButton.value]?.[1] ?? []);
 
 const deleteWord = (p: number[]) => p.forEach((n) => (letters.value[n] = ''));
 
-const parseInput = (n: number) => {
-	const char = letters.value[n]!;
-	if (persianMap[char]) letters.value[n] = persianMap[char];
-
-	if (!/[ا-ی]/.test(letters.value[n]!)) letters.value[n] = '';
-};
-
-watch(
-	letters.value,
-	() => {
-		const emptyLetterIdx = letters.value.indexOf('');
-		if (emptyLetterIdx >= 0) {
-			inputs.value?.[emptyLetterIdx]?.focus();
-			focusedButton.value = 0;
-		}
-	},
-	{ immediate: true },
-);
+watch(letters.value, (v) => {
+	if (v.indexOf('') >= 0) selectedButton.value = 0;
+});
 
 onKeyStroke(['Control'], () => deleteWord(range(16)));
 onKeyStroke(['Enter'], () => deleteWord(path.value));
-onKeyStroke(['Shift'], () => focusedButton.value++);
+onKeyStroke(['Shift'], () => selectedButton.value++);
 </script>
 <template>
 	<div class="flex items-center justify-center flex-col h-dvh gap-3">
@@ -43,28 +26,15 @@ onKeyStroke(['Shift'], () => focusedButton.value++);
 				v-if="words.length"
 				@click="deleteWord(range(16))"
 			/>
-			<div
-				class="grid grid-cols-4 bg-primary/60 rounded-lg p-4 gap-2 w-fit mx-auto"
-			>
-				<input
-					v-for="n in range(16)"
-					v-model="letters[n]"
-					ref="inputs"
-					ref_for
-					:key="n"
-					class="rounded-md border border-black/30 w-14 h-12 text-center transition-all hover:opacity-80"
-					:class="path.includes(n) ? 'bg-gray-700' : 'bg-gray-800'"
-					maxlength="1"
-					@click="deleteWord([n])"
-					@keyup.delete="deleteWord(letters[n] ? [n] : [n - 1])"
-					@input="nextTick(() => parseInput(n))"
-				/>
-			</div>
+			<TextFields
+				v-model="letters"
+				:path
+			/>
 			<div class="grid grid-cols-3 px-4 gap-2 mx-auto pb-4">
 				<UIWord
 					v-for="([word, p, score], i) in words"
 					:key="word"
-					@click="isEqual(path, p) ? deleteWord(p) : (focusedButton = i)"
+					@click="isEqual(path, p) ? deleteWord(p) : (selectedButton = i)"
 					:word
 					:score
 					:selected="isEqual(path, p)"
