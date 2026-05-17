e79dc2d08a55c52718593e1e15d168c12c9ee04d
Navid Talebian <navidt81@gmail.com>
2026-05-17 18:43:35 +0330

    improve UI

--- app/components/UI/deleteButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<button
-		class="w-full max-w-62 block bg-red-700/80 rounded-md py-2 hover:opacity-80 transition-all cursor-pointer"
-	>
-		پاک
-	</button>
-</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -35,32 +35,41 @@ onKeyStroke(['Enter'], () => deleteWord(path.value));
 onKeyStroke(['Shift'], () => focusedButton.value++);
 </script>
 <template>
-	<div class="flex items-center justify-center flex-col py-12 h-dvh gap-3">
-		<div class="grid grid-cols-4 bg-primary/60 rounded-md p-2 gap-2">
-			<input
-				v-for="n in range(16)"
-				v-model="letters[n]"
-				ref="inputs"
-				ref_for
-				:key="n"
-				class="rounded-md border border-black/30 w-14 h-12 text-center transition-all hover:opacity-80"
-				:class="path.includes(n) ? 'bg-gray-700' : 'bg-gray-800'"
-				maxlength="1"
-				@click="deleteWord([n])"
-				@keyup.delete="deleteWord(letters[n] ? [n] : [n - 1])"
-				@input="nextTick(() => parseInput(n))"
+	<div class="flex items-center justify-center flex-col h-dvh gap-3">
+		<div
+			class="border-px border-white/20 rounded-md w-full max-w-xl relative flex flex-col justify-between bg-[#111312]"
+		>
+			<UIReload
+				v-if="words.length"
+				@click="deleteWord(range(16))"
 			/>
-		</div>
-		<UIDeleteButton @click="deleteWord(range(16))" />
-		<div class="grid grid-cols-3 gap-2">
-			<UIWord
-				v-for="([word, p, score], i) in words"
-				:key="word"
-				@click="isEqual(path, p) ? deleteWord(p) : (focusedButton = i)"
-				:word
-				:score
-				:selected="isEqual(path, p)"
-			/>
+			<div
+				class="grid grid-cols-4 bg-primary/60 rounded-lg p-4 gap-2 w-fit mx-auto"
+			>
+				<input
+					v-for="n in range(16)"
+					v-model="letters[n]"
+					ref="inputs"
+					ref_for
+					:key="n"
+					class="rounded-md border border-black/30 w-14 h-12 text-center transition-all hover:opacity-80"
+					:class="path.includes(n) ? 'bg-gray-700' : 'bg-gray-800'"
+					maxlength="1"
+					@click="deleteWord([n])"
+					@keyup.delete="deleteWord(letters[n] ? [n] : [n - 1])"
+					@input="nextTick(() => parseInput(n))"
+				/>
+			</div>
+			<div class="grid grid-cols-3 px-4 gap-2 mx-auto pb-4">
+				<UIWord
+					v-for="([word, p, score], i) in words"
+					:key="word"
+					@click="isEqual(path, p) ? deleteWord(p) : (focusedButton = i)"
+					:word
+					:score
+					:selected="isEqual(path, p)"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
