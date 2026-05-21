1b6ec0025adbe3d1d103be6e28b75774fd164ca8
Navid Talebian <navidt81@gmail.com>
2026-05-21 13:30:09 +0330

    remove useChar composable

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
+import { chunk, fill } from 'es-toolkit';
+
 const aspect = ref<[number, number]>([4, 4]);
+const chars = ref(fill(Array(aspect.value[0] * aspect.value[1]), ''));
+
+const { data, execute, clear } = useFetch('/api/search', {
+	onRequest: ({ options }) => {
+		options.body = { grid: chunk(chars.value, aspect.value[0]) };
+	},
+	immediate: false,
+	method: 'post',
+	watch: false,
+});
+
+const words = computed(() => data.value?.words);
 
 const selected = ref(0);
-const { chars, words } = useChars(aspect);
 
 const submit = (path?: number[]) => {
 	if (path) path.forEach((n) => (chars.value[n] = ''));
 	else chars.value.fill('');
 	selected.value = 0;
 };
+watch(chars.value, (v) => (v.indexOf('') == -1 ? execute() : clear()));
 </script>
 <template>
 	<Box>
