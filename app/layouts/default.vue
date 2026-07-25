28b61a0df40652fe726ccf7c11e215b221d850b3
Navid Talebian <navidt81@gmail.com>
2026-07-25 11:00:32 +0330

    simplify project

--- app/layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div class="flex items-center justify-center flex-col h-dvh gap-3 p-4">
-		<slot />
-	</div>
-</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { chunk, fill } from 'es-toolkit';
+import { chunk } from 'es-toolkit';
 
 const maxResults = ref(12);
 const aspect = ref<[number, number]>([4, 4]);
-const chars = ref(fill(Array(aspect.value[0] * aspect.value[1]), ''));
+const chars = ref(Array(aspect.value[0] * aspect.value[1]).fill(''));
 
 const body = computed(() => ({
 	grid: chunk(chars.value, aspect.value[0]),
@@ -23,25 +23,40 @@ const path = computed(() => words.value?.[selected.value]?.path);
 const selected = ref(0);
 
 const submit = () => path.value?.forEach((n) => (chars.value[n] = ''));
+
+onKeyStroke(['Control'], () => chars.value.fill(''));
 </script>
 <template>
-	<Box>
-		<template #toolbar>
-			<Settings
-				v-model:aspect="aspect"
-				v-model:max-results="maxResults"
+	<div class="flex items-center justify-center flex-col h-dvh gap-3 p-4">
+		<div
+			class="border-px border-white/20 rounded-md w-full max-w-xl p-4 relative flex flex-col justify-between bg-[#111312]"
+		>
+			<div class="absolute top-4 left-4 text-xs p-1 flex gap-4">
+				<Settings
+					v-model:aspect="aspect"
+					v-model:max-results="maxResults"
+				/>
+				<Icon
+					class="cursor-pointer hover:opacity-80 transition-all"
+					name="lucide:refresh-ccw"
+					size="18"
+					@click="chars.fill('')"
+				/>
+			</div>
+
+			<div class="h-10"></div>
+
+			<FieldTable
+				v-model="chars"
+				:aspect
+				:path
 			/>
-			<Refresh @click="chars.fill('')" />
-		</template>
-		<FieldTable
-			v-model="chars"
-			:aspect
-			:path
-		/>
-		<WordTable
-			v-model="selected"
-			:words
-			@submit="submit"
-		/>
-	</Box>
+			<WordTable
+				v-model="selected"
+				:words
+				@submit="submit"
+			/>
+			<div class="h-10"></div>
+		</div>
+	</div>
 </template>
