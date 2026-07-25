94944e2e1e4ad551985d57494ade43c07536243d
Navid Talebian <navidt81@gmail.com>
2026-07-25 10:46:17 +0330

    delete tauri config on nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,12 +10,6 @@ export default defineNuxtConfig({
 	ssr: false,
 
 	css: ['~/assets/css/main.css'],
-	telemetry: false,
-
-	devServer: {
-		host: '0',
-		port: 3001,
-	},
 
 	experimental: {
 		viteEnvironmentApi: true,
@@ -24,16 +18,8 @@ export default defineNuxtConfig({
 	},
 
 	vite: {
-		clearScreen: false,
 		optimizeDeps: {
 			include: ['es-toolkit'],
 		},
-		envPrefix: ['VITE_', 'TAURI_'],
-		server: {
-			// Tauri requires a consistent port
-			strictPort: true,
-		},
 	},
-
-	ignore: ['**/src-tauri/**'],
 });
